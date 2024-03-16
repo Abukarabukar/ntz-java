cecfe7c757ffe2eb19407358ed9b845fe4bc05eb
Abukarabukare <Abukarshegow@gmail.comm>
2024-03-16 10:22:35 -0400

    Not yet

--- ntz/src/main/java/rocks/zipcode/NoteList.java
+++ ntz/src/main/java/rocks/zipcode/NoteList.java
@@ -8,7 +8,7 @@ public class NoteList extends ArrayList<String> {
      */
     private static final long serialVersionUID = 1L;
 
-    // yes, really, it's just a no-additional functionality subclass.
+    // 1yes, really, it's just a no-additional functionality subclass.
     public NoteList(String s) {
         super();
         this.add(s);
